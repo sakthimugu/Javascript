e0f48a880c97362a4e6ad2f2fa605b99e617841d
Mugunthan M <mugu.7533@gmail.com>
2025-01-23 13:44:32 +0530

    Add files via upload

--- Jan 22 Loop Task/script.js
+++ Jan 22 Loop Task/script.js
@@ -7,6 +7,7 @@ for(num;num<=10;num++){
     console.log("The numbers are",num);
 }
 
+
 //task2 Even Numbers from 1 to 20
 console.log("Task 2 : ");
 let num1=1;
@@ -15,6 +16,8 @@ for(num1;num1<=20;num1++){
         console.log("The Even numbers are",num1);
     } 
 }
+
+
 
 //task3 Odd Numbers from 1 to 20
 console.log("Task 3 : ");
@@ -26,6 +29,8 @@ for(num2;num2<=20;num2++){
 }
 
 
+
+
 // task4 Sum of Numbers from 1 to 10
 console.log("Task 4 : ");
 let start = 0;
@@ -35,6 +40,8 @@ for(let i=0;i<=sum;i++){
 }
 console.log("The sum of",sum,"is",start);
 
+
+
 //task5 Factorial Number
 console.log("Task 5 :");
 let factorial = 1;
@@ -43,6 +50,8 @@ for(a=1;a<=givenFact;a++){
     factorial = factorial*a;   
 }
 console.log("The Factorial Number",givenFact,"is",factorial);
+
+
 
 //task6 Multiplication table of 5
 console.log("Task 6 : ");
@@ -54,7 +63,20 @@ for(k;k<=givenTable;k++){
 }
 
 
+
+
 // task7 Fibonacci Series
+console.log("Task 7:");
+let first = 0;
+let second = 1;
+console.log(first);
+console.log(second);
+for(let i=2;i<10;i++){
+    let Fibonacci = first + second;
+    console.log(Fibonacci);
+    first=second;
+    second=Fibonacci;
+}
 
 
 
@@ -66,15 +88,22 @@ for(reverse;reverse>=1;reverse--){
     console.log("The Numbers are",reverse);
 }
 
-//task9 Prime Numbers
-// console.log("Task 9 :");
-// let prime = 1;
-// let givennumber = 10;
-// for(prime;prime<=givennumber;prime++){
-//     if(prime%1==0 && prime%prime==0){  //doubt
-//         console.log(prime);
-//     }
-// }
+
+
+
+// task9 Prime Numbers
+console.log("Task 9 :");
+let givennumber = 20;
+for(p=2;p<=givennumber;p++){
+    if(p<=3){
+        console.log(p);
+    }
+    else if(p%2!==0 && p%3!==0){
+        console.log(p);
+    }
+}  
+
+
 
 
 //task 10 Print numbers from 1 to 100
@@ -89,13 +118,17 @@ for(z;z<=100;z++){
 
 
 //While loop
-console.log("While Loop");
+
+
 //task 1 Print Numbers from 1 to 10
+console.log("While Loop");
 let a1=1;
 while(a1<=10){
     console.log("The numbers are",a1);
     a1++;
 }
+
+
 
 //task2 Print even numbers from 1 to 20
 console.log("Task 2 :");
@@ -107,6 +140,8 @@ while(b1<=20){
     b1++;
 }
 
+
+
 //task3 Print odd numbers from 1 to 20
 console.log("Task 3 :");
 let c1=1;
@@ -116,6 +151,9 @@ while(c1<=20){
     }
     c1++;
 }
+
+
+
 
 // task5 Sum of Numbers from 1 to 10
 console.log("Task 4 : ");
@@ -128,6 +166,9 @@ while(d3<=d2){
 }
 console.log("The sum of",d2,"is",d1);
 
+
+
+
 //task5 Factorial of a number
 console.log("Task 5 :");
 let e1 = 1;
@@ -138,5 +179,77 @@ while(e3<=e2){
     e3++;
 }
 console.log("The Factorial of",e2,'is',e1);
+
+
+
+
+//task6 Multiple of 5
+console.log("Task 6 :");
+let table5 = 1;
+let end = 10;
+while(table5<=10){
+    console.log(`${table5}*5=${table5*5}`);
+    table5++;  
+}
+
+
+
+
+//task 7 Fibonacci Series
+console.log("Task 7 :");
+let f1=0;
+let f2=1;
+let f3=2
+console.log(f1);
+console.log(f2);
+while (f3<10) {
+    let f4=f1+f2;
+    console.log(f4);
+    f1=f2;
+    f2=f4;
+    f3++;
+}
+
+
+
+//task8 Print number from 10 to 1
+console.log("Task 8 :");
+let started = 10;
+while (started>=1) {
+    console.log(started);
+    started--;
+}
+
+
+
+//task9 Prime Numbers
+console.log("Task 9 :");
+let givennumbers = 20;
+let primeNumber=2;
+while(primeNumber<=givennumbers){
+    if(primeNumber<=3){
+        console.log(primeNumber);
+    }
+    else if(primeNumber%2!==0 && primeNumber%3!==0){
+        console.log(primeNumber);
+    }
+    primeNumber++;
+}
+
+
+
+//task10 Print numbers from 1 to 100
+console.log("Task 10 :");
+let q = 0;
+let w = 100;
+while (q<w) {
+    q++;
+    if(q%3!==0){
+        console.log(q);
+    }
+}
+
+
+  
 
 
