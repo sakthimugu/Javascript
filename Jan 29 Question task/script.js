ac57ff566239edd6c3a9a5ca1eb1e1a5f705ce0b
Mugunthan M <mugu.7533@gmail.com>
2025-01-30 18:27:32 +0530

    Add files via upload

---
+++ Jan 29 Question task/script.js
@@ -0,0 +1,76 @@
+//Question 1
+// let Q1 = 0;//20
+// while (Q1<20){
+//     Q1=Q1+1;
+//     console.log(Q1);
+    
+// }
+
+//Question 2
+// let count = 7;
+// function updatecount(){
+//     let count = 5;
+//     count++; 
+// }
+// console.log(count);
+// updatecount()
+
+
+//Question 3
+// const numbers = [1,2,3,4,5]
+// for(const num of number){
+//     console.log(num);
+// }
+
+
+//Question 4
+// let stringvalue = "Hellow";
+// stringvalue[0] = "Y";
+// console.log(stringvalue);
+
+
+//Question 5
+// const firstname = "Mugunthan";
+// const man = {
+//     firstname : "Sakthi",
+//     age : 21
+// }
+// console.log(firstname);
+
+
+//Question 6
+// const q6 = NaN;
+// if(q6===NaN){
+//     console.log("Nan");
+// }
+// else{
+//     console.log("Not a Nan");   
+// }
+
+//Question 7
+// const arrayy = ['one','two','three'];
+// for(const arr in arrayy){
+//     console.log(arr);
+// }
+// for(const arr of arrayy){
+//     console.log(arr);   
+// }
+
+//Question 8
+// const firstObj = {
+//     name : "Mugunthan",
+//     age : 21
+// }
+// const secondObj = {
+//     name : "Mugunthan",
+//     age : 21
+// }
+// console.log(firstObj===secondObj);
+
+//Question 9
+// let user = null;
+// console.log(typeof user);
+
+// Question 10
+// let fruits = 'b'+'a'+ +'a'+'a';
+// console.log(fruits)
